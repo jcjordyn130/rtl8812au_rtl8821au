466f84f500c8b1370cf65059dd99efcf1b15ff2d
Jordyn Carattini <jordyncarattini@outlook.com>
2016-06-28 20:35:58 -0500

    Auto install script
    A little script to clone this repo and install it

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# root check
+if [  "$(id -u)" != "0" ]
+then
+ echo "Must be root."
+ exit 1
+fi
+
+# install deps
+apt install git linux-headers-generic build-essential -y
+
+# install driver
+cd /tmp
+mkdir wireless_driver
+cd wireless_driver
+git clone https://github.com/Grawp/rtl8812au_rtl8821au.git
+cd rtl8812au_rtl8821au
+nice make -j$(nproc)
+make install
+modprobe -v 8812au
